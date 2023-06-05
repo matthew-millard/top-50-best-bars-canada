6385ff87141f38c70d78fe52ea4e38955155b9c9
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2023-06-05 14:49:58 -0400

    Build: create bars model

---
+++ models/bars.js
@@ -0,0 +1,51 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Bars extends Model {}
+
+Bars.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    bar_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    patio: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    website: {
+      type: DataTypes.STRING,
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    province_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Provinces',
+        key: 'id',
+      },
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'Bars',
+  }
+);
+
+// Export
+module.exports = Bars;
