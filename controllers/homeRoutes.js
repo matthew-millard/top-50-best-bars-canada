fa9d564f3f509338f8b545fd4952ac3e4fd93ae2
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2023-06-05 18:21:34 -0400

    Test: routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,10 +1,30 @@
 // Imports
 const home = require('express').Router();
+const { Bars, HistoricalRanks, Provinces } = require('../models');
 
 // GET Homepage
 home.get('/', async (req, res) => {
   try {
-    return res.status(200).render('layouts/main');
+    const barData = await Bars.findAll({
+      include: [
+        {
+          model: Provinces,
+          attributes: ['province_name'],
+        },
+        {
+          model: HistoricalRanks,
+          attributes: ['rank_position', 'year'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const bars = barData.map((bar) => bar.get({ plain: true }));
+    console.table(bars);
+    // Pass serialized data and session flag into template
+    res.render('homepage', {
+      bars,
+    });
   } catch (err) {
     console.error(err);
     return res.status(500).json({ message: 'Server Error' });
