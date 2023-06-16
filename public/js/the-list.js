e95721eb27c9c93670657b4adabec4082bef670f
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2023-06-16 15:55:08 -0400

    Style: landing page and the list page

--- public/js/the-list.js
+++ public/js/the-list.js
@@ -1,5 +1,6 @@
 const moreInfoBtn = Array.from(document.querySelectorAll('.more-info-btn'));
 
+
 moreInfoBtn.forEach((button) => {
   button.addEventListener('click', async () => {
     const id = button.getAttribute('data-id');
--- public/js/welcome-page.js
+++ public/js/welcome-page.js
@@ -1,4 +1,8 @@
 const cta = document.querySelector('.welcome-cta');
+const header = document.querySelector('.header');
+
+// Add display none from header
+header.classList.add('display-none');
 
 cta.addEventListener('click', () => {
   document.location.replace('/the-list');
