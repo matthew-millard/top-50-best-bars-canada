d3951d9efbfb9b9120fb9d01ba9efd0b651793c8
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2023-06-16 23:35:38 -0400

    Feat: Search bars in your area

---
+++ public/js/search-results.js
@@ -0,0 +1,30 @@
+const bars = Array.from(document.querySelectorAll('.search-result-bar'));
+
+bars.forEach((bar) => {
+  bar.addEventListener('click', (event) => {
+    event.stopPropagation();
+    const barId = event.currentTarget.getAttribute('data-bar-id');
+    const url = `api/bars/${barId}`;
+    redirect(url, barId);
+  });
+});
+
+// Redirect to bar page
+async function redirect(url, barId) {
+  console.log(barId);
+  try {
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (!response.ok) {
+      console.error(response);
+      return document.location.replace('/search');
+    }
+    document.location.replace(`/api/bars/${barId}`);
+  } catch (err) {
+    console.error(err);
+  }
+}
